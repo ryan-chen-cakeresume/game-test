a4f53a22e6842443cc114f754b4b789f5dbb0665
Ryan Chen <96723490+ryan-chen-cakeresume@users.noreply.github.com>
2025-04-11 11:04:06 +0800

    Pageview

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,26 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-RBF3K2F49W"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-RBFx3K2F49W');
+            `
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
